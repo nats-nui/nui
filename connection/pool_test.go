2576e00a63d9f2b1e79adc6ea578fb08a3352f8c
pricelessrabbit <pricelessrabbit@gmail.com>
2023-07-05 22:33:40 +0200

    updated pool to use generics

--- connection/pool_test.go
+++ connection/pool_test.go
@@ -2,7 +2,6 @@ package connection
 
 import (
 	"fmt"
-	"github.com/nats-io/nats.go"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
@@ -14,29 +13,31 @@ type ConnMock struct {
 func (m *ConnMock) Close() {
 }
 
-func mockBuilder(connection *Connection) (*nats.Conn, error) {
-	return &nats.Conn{}, nil
+func mockBuilder(connection *Connection) (*ConnMock, error) {
+	return &ConnMock{}, nil
+}
+
+var repo ConnRepo
+var pool *ConnPool[*ConnMock]
+
+func TestMain(m *testing.M) {
+	repo = NewMemConnRepo()
+	pool = NewConnPool[*ConnMock](repo, mockBuilder)
+	_ = repo.Save(&Connection{Id: "c1"})
+	m.Run()
 }
 
 func TestConnPool_Get(t *testing.T) {
-	r := NewMemConnRepo()
-	r.Save(&Connection{Id: "c1"})
-	p := NewConnPool(r)
-	p.build = mockBuilder
-	c, err := p.Get("c1")
+	c, err := pool.Get("c1")
 	require.Nil(t, err)
-	c2, _ := p.Get("c1")
+	c2, _ := pool.Get("c1")
 	require.Equal(t, c, c2)
 }
 
 func TestConnPool_Refresh(t *testing.T) {
-	r := NewMemConnRepo()
-	r.Save(&Connection{Id: "c1"})
-	p := NewConnPool(r)
-	p.build = mockBuilder
-	c, _ := p.Get("c1")
-	p.Refresh("c1")
-	c2, _ := p.Get("c1")
+	c, _ := pool.Get("c1")
+	pool.Refresh("c1")
+	c2, _ := pool.Get("c1")
 	fmt.Println(c, c2)
 	require.NotSame(t, c, c2)
 }
