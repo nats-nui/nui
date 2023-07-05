f3f0f7755addc1c34f55e5c930b024840a38ab7d
pricelessrabbit <pricelessrabbit@gmail.com>
2023-07-05 22:33:40 +0200

    updated pool to use generics

--- connection/pool.go
+++ connection/pool.go
@@ -11,52 +11,58 @@ type Conn interface {
 	Close()
 }
 
-type builder = func(connection *Connection) (Conn, error)
-
-type ConnPool struct {
-	m     sync.Mutex
-	conns map[string]Conn
-	repo  ConnRepo
-	build builder
+type Pool interface {
+	Get(id string) (Conn, error)
+	Refresh(id string) error
 }
 
-func NewConnPool(repo ConnRepo) *ConnPool {
-	return &ConnPool{
-		conns: make(map[string]Conn),
+type ConnPool[T Conn] struct {
+	m     sync.Mutex
+	conns map[string]T
+	repo  ConnRepo
+	build func(connection *Connection) (T, error)
+}
+
+func NewConnPool[T Conn](repo ConnRepo, builder func(connection *Connection) (T, error)) *ConnPool[T] {
+	return &ConnPool[T]{
+		conns: make(map[string]T),
 		repo:  repo,
-		build: natsBuilder,
+		build: builder,
 	}
 }
 
-func (p *ConnPool) Get(id string) (Conn, error) {
+func NewNatsConnPool(repo ConnRepo) *ConnPool[*nats.Conn] {
+	return NewConnPool[*nats.Conn](repo, natsBuilder)
+}
+
+func (p *ConnPool[T]) Get(id string) (T, error) {
 	p.m.Lock()
 	defer p.m.Unlock()
-	if _, ok := p.conns[id]; !ok {
+	if t, ok := p.conns[id]; !ok {
 		err := p.refresh(id)
 		if err != nil {
-			return nil, err
+			return t, err
 		}
 	}
-	if c, ok := p.conns[id]; ok {
+	c, ok := p.conns[id]
+	if ok {
 		return c, nil
 	}
-	return nil, errors.New("cannot find connection in pool")
+	return c, errors.New("cannot find connection in pool")
 }
 
-func (p *Connp)
-
-func (p *ConnPool) Refresh(id string) error {
+func (p *ConnPool[T]) Refresh(id string) error {
 	p.m.Lock()
 	defer p.m.Unlock()
 	return p.refresh(id)
 }
 
-func (p *ConnPool) refresh(id string) error {
+func (p *ConnPool[T]) refresh(id string) error {
 	c, err := p.repo.GetById(id)
 	if err != nil {
 		return err
 	}
-	if currentConn := p.conns[id]; currentConn != nil {
+	if currentConn, ok := p.conns[id]; ok {
 		currentConn.Close()
 	}
 	conn, err := p.build(c)
@@ -67,10 +73,6 @@ func (p *ConnPool) refresh(id string) error {
 	return nil
 }
 
-func (p *ConnPool) setBuilder(b builder) {
-	p.build = b
-}
-
-func natsBuilder(connection *Connection) (Conn, error) {
+func natsBuilder(connection *Connection) (*nats.Conn, error) {
 	return nats.Connect(strings.Join(connection.Hosts, ", "))
 }
