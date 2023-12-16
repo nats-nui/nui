700308c409aadeba0fa99bf11894ccd655864db2
pricelessrabbit <pricelessrabbit@gmail.com>
2023-12-16 10:45:15 +0100

    pretty print json test

--- web/src/components/stacks/message/MessageView.tsx
+++ web/src/components/stacks/message/MessageView.tsx
@@ -3,7 +3,13 @@ import Header from "@/components/Header"
 import { MessageState, MessageStore } from "@/stores/stacks/message"
 import { useStore } from "@priolo/jon"
 import React, { FunctionComponent } from "react"
-
+import JsonView from '@uiw/react-json-view';
+import JsonViewEditor from '@uiw/react-json-view/editor';
+import { lightTheme } from '@uiw/react-json-view/light';
+import { darkTheme } from '@uiw/react-json-view/dark';
+import { monokaiTheme } from '@uiw/react-json-view/monokai';
+import { TriangleArrow } from '@uiw/react-json-view/triangle-arrow';
+import { TriangleSolidArrow } from '@uiw/react-json-view/triangle-solid-arrow';
 
 
 interface Props {
@@ -18,6 +24,34 @@ const MessageView: FunctionComponent<Props> = ({
 
 	// STORE
 	const msgSa = useStore(msgSo) as MessageState
+	const example = {"widget": {
+			"debug": "on",
+			"window": {
+				"title": "Sample Konfabulator Widget",
+				"name": "main_window",
+				"width": 500,
+				"height": 500
+			},
+			"image": {
+				"src": "Images/Sun.png",
+				"name": "sun1",
+				"hOffset": 250,
+				"vOffset": 250,
+				"alignment": "center"
+			},
+			"text": {
+				"data": "Click Here",
+				"size": 36,
+				"style": "bold",
+				"name": "text1",
+				"hOffset": 250,
+				"vOffset": 100,
+				"alignment": "center",
+				"onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"
+			}
+		}}
+
+	const customTheme = {...monokaiTheme, '--w-rjv-background-color': "#3E3E3E"}
 
 	// HOOKs
 	
@@ -26,8 +60,9 @@ const MessageView: FunctionComponent<Props> = ({
 	// RENDER
 	
 	return (
-		<div style={{ ...cssContainer, ...style }}>
+		<div>
 			<Header view={msgSo} icon={<img src={imgMsg} />} />
+			<JsonView value={example} style={customTheme} />
 		</div>
 	)
 }
