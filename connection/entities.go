541a813a91d610bed33a9864e49ad00093b971d6
pricelessrabbit <pricelessrabbit@gmail.com>
2023-12-17 17:10:26 +0100

    added connection auth to api

--- connection/entities.go
+++ connection/entities.go
@@ -1,12 +1,11 @@
 package connection
 
 type Connection struct {
-	Id            string         `json:"id" clover:"id"`
-	Name          string         `json:"name" clover:"name"`
-	Hosts         []string       `json:"hosts" clover:"hosts"`
-	Subscriptions []Subscription `json:"subscriptions" clover:"subscriptions"`
-	Auth          Auth           `json:"Auth" clover:"Auth"`
-	Auth2         []Auth         `json:"auth2" clover:"auth2"`
+	Id            string         `json:"id" `
+	Name          string         `json:"name" `
+	Hosts         []string       `json:"hosts" `
+	Subscriptions []Subscription `json:"subscriptions" `
+	Auth          []Auth         `json:"auth" `
 }
 
 type Subscription struct {
@@ -20,13 +19,13 @@ const AuthModeJwt = "auth_jwt"
 const AuthModeCredsFile = "auth_creds_file"
 
 type Auth struct {
-	Mode          string `json:"Mode" clover:"Mode"`
-	Username      string `json:"username" clover:"username"`
-	Password      string `json:"password" clover:"password"`
-	Token         string `json:"token" clover:"token"`
-	Jwt           string `json:"jwt" clover:"jwt"`
-	NKeySeed      string `json:"nkey" clover:"nkey"`
-	CredsFilePath string `json:"creds" clover:"creds"`
+	Mode     string `json:"mode" `
+	Username string `json:"username" `
+	Password string `json:"password" `
+	Token    string `json:"token" `
+	Jwt      string `json:"jwt" `
+	NKeySeed string `json:"n_key_seed"`
+	Creds    string `json:"creds"`
 }
 
 type ConnStatusChanged struct {
