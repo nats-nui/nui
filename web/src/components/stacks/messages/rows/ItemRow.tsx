daff3d3405090e03c9378c89fb63bbfeb5a572da
ivano.iorio <ivano.iorio@zerofarms.it>
2024-02-03 08:36:29 +0100

    semplify MessageRow

--- web/src/components/stacks/messages/rows/ItemRow.tsx
+++ web/src/components/stacks/messages/rows/ItemRow.tsx
@@ -1,9 +1,15 @@
-import layoutSo from "@/stores/layout"
+import TooltipWrapCmp from "@/components/TooltipWrapCmp"
+import IconButton from "@/components/buttons/IconButton"
+import Base64Cmp from "@/components/formatters/base64/Base64Cmp"
+import HexTable from "@/components/formatters/hex/HexTable"
+import JsonRow from "@/components/formatters/json/JsonRow"
+import TextRow from "@/components/formatters/text/TextRow"
+import CopyIcon from "@/icons/CopyIcon"
+import layoutSo, { COLOR_VAR } from "@/stores/layout"
 import { MSG_FORMAT } from "@/stores/stacks/messages/utils"
 import { Message } from "@/types/Message"
 import dayjs from "dayjs"
-import { FunctionComponent, useMemo } from "react"
-import MessageRow from "./MessageRow"
+import { FunctionComponent, useMemo, useState } from "react"
 
 
 
@@ -22,16 +28,24 @@ const ItemRow: FunctionComponent<Props> = ({
 }) => {
 
 	// STORE
+	const [bttCopyVisible, setBttCopyVisible] = useState(false)
 
 	// HOOKs
 
 	// HANDLER
 	const handleClick = () => onClick?.(message)
+	const handleClipboardClick = (e: React.MouseEvent<Element, MouseEvent>) => {
+		e.preventDefault()
+		e.stopPropagation()
+		navigator.clipboard.writeText(message.payload)
+	}
+	const handleTitleEnter = () => setBttCopyVisible(true)
+	const handleTitleLeave = () => setBttCopyVisible(false)
 
 	// RENDER
 	if (!message) return null
 	const time = useMemo(
-		() => !!message.receivedAt ? dayjs(message.receivedAt).format("YYYY-MM-DD HH:mm:ss") : "",
+		() => message.receivedAt ? dayjs(message.receivedAt).format("YYYY-MM-DD HH:mm:ss") : null,
 		[message.receivedAt]
 	)
 
@@ -39,11 +53,31 @@ const ItemRow: FunctionComponent<Props> = ({
 		<div style={cssRoot(index)}
 			onClick={handleClick}
 		>
-			<MessageRow message={message} format={format} />
-
-			<div style={cssFooter}>
-				{time}
+			<div style={cssTitle}
+				onMouseEnter={handleTitleEnter}
+				onMouseLeave={handleTitleLeave}
+			>
+				<div style={{ display: "flex", flex: 1 }}>
+					{message.seqNum && <div style={{ width: 20 }}>{message.seqNum}</div>}
+					<div style={cssTitleText}>{message.subject}</div>
+				</div>
+				{bttCopyVisible && (
+					<TooltipWrapCmp content="COPY" variant={COLOR_VAR.CYAN}>
+						<IconButton onClick={handleClipboardClick}>
+							<CopyIcon />
+						</IconButton>
+					</TooltipWrapCmp>
+				)}
 			</div>
+
+			{{
+				[MSG_FORMAT.JSON]: <JsonRow text={message.payload} />,
+				[MSG_FORMAT.TEXT]: <TextRow text={message.payload} />,
+				[MSG_FORMAT.BASE64]: <Base64Cmp text={message.payload} maxChar={80} />,
+				[MSG_FORMAT.HEX]: <HexTable text={message.payload} maxRows={10} />,
+			}[format]}
+
+			{time && <div style={cssFooter}>{time}</div>}
 		</div>
 	)
 }
@@ -56,6 +90,19 @@ const cssRoot = (index: number): React.CSSProperties => ({
 	flexDirection: "column",
 	padding: "0px 7px 5px 7px",
 })
+
+const cssTitle: React.CSSProperties = {
+	display: "flex",
+	alignItems: "center",
+	height: 24,
+	fontSize: 10,
+}
+
+const cssTitleText: React.CSSProperties = {
+	flex: 1,
+	color: layoutSo.state.theme.palette.default.fg2,
+}
+
 const cssFooter: React.CSSProperties = {
 	fontSize: 10,
 	opacity: .7,
