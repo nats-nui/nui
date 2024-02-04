46d9517348c4883b653587c98668e37cf3a271dd
pricelessrabbit <pricelessrabbit@gmail.com>
2024-02-04 19:27:40 +0100

    added data and handler in key value mocks

--- web/src/mocks/ajax/handlers/index.ts
+++ web/src/mocks/ajax/handlers/index.ts
@@ -1,10 +1,11 @@
 import connection from "./connection"
 import stream from "./stream"
 import consumer from "./consumer"
-
+import keyValueEntry from "./keyValueEntry.ts"
 
 export const handlers = [
 	...connection,
 	...stream,
 	...consumer,
+	...keyValueEntry
 ]
---
+++ web/src/mocks/data/keyValueEntries.ts
@@ -0,0 +1,94 @@
+export default [
+    {
+        "key": "0",
+        "payload": null,
+        "last_update": "2023-11-14T21:45:32+01:00",
+        "operation": "KeyValuePutOp",
+        "revision": 1,
+        "is_deleted": false,
+        "history": null
+    },
+    {
+        "key": "1",
+        "payload": null,
+        "last_update": "2023-11-14T21:45:32+01:00",
+        "operation": "KeyValuePutOp",
+        "revision": 2,
+        "is_deleted": false,
+        "history": null
+    },
+    {
+        "key": "2",
+        "payload": null,
+        "last_update": "2023-11-14T21:45:32+01:00",
+        "operation": "KeyValuePutOp",
+        "revision": 3,
+        "is_deleted": false,
+        "history": null
+    },
+    {
+        "key": "3",
+        "payload": null,
+        "last_update": "2023-11-14T21:45:33+01:00",
+        "operation": "KeyValuePutOp",
+        "revision": 4,
+        "is_deleted": false,
+        "history": null
+    },
+    {
+        "key": "4",
+        "payload": null,
+        "last_update": "2023-11-14T21:45:33+01:00",
+        "operation": "KeyValuePutOp",
+        "revision": 5,
+        "is_deleted": false,
+        "history": null
+    },
+    {
+        "key": "5",
+        "payload": null,
+        "last_update": "2023-11-14T21:45:33+01:00",
+        "operation": "KeyValuePutOp",
+        "revision": 6,
+        "is_deleted": false,
+        "history": null
+    },
+    {
+        "key": "6",
+        "payload": null,
+        "last_update": "2023-11-14T21:45:33+01:00",
+        "operation": "KeyValuePurgeOp",
+        "revision": 7,
+        "is_deleted": false,
+        "history": null
+    },
+    {
+        "key": "7",
+        "payload": null,
+        "last_update": "2023-11-14T21:45:33+01:00",
+        "operation": "KeyValueDeleteOp",
+        "revision": 8,
+        "is_deleted": false,
+        "history": null
+    },
+]
+
+export const keyValueEntryDetail: any = {
+    "key": "host.name",
+    "payload": "dmFsdWU=",
+    "last_update": "2022-11-17T04:28:29Z",
+    "operation": "KeyValuePutOp",
+    "revision": 2,
+    "is_deleted": false,
+    "history": [
+        {
+            "key": "host.name",
+            "payload": "dmFsdWU=",
+            "last_update": "2022-11-17T05:28:29+01:00",
+            "operation": "KeyValuePutOp",
+            "revision": 2,
+            "is_deleted": false,
+            "history": null
+        }
+    ]
+}
