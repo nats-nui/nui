ebb833e805feb43675c0e3743c9784631b33a949
pricelessrabbit <pricelessrabbit@gmail.com>
2024-02-20 21:46:45 +0100

    fixed number input to test bucket creation

--- frontend/src/components/input/NumberInput.tsx
+++ frontend/src/components/input/NumberInput.tsx
@@ -7,12 +7,14 @@ interface Props extends TextInputProps {
 	min?: number,
 	max?: number,
 	decimals?: number,
+	onNumberChange?: (value: number) => void
 }
 
 const NumberInput: FunctionComponent<Props> = ({
-	min,
+    decimals,
+    min,
 	max,
-	decimals,
+    isInt,
 	...props
 }) => {
 
@@ -22,19 +24,24 @@ const NumberInput: FunctionComponent<Props> = ({
 
 	// HANDLER
 	const handleChange = (newValue:string) => {
-		const value = format(newValue, decimals, min, max)
-		props.onChange?.(value)
+		const valueNum = format(newValue, decimals, min, max, isInt)
+		props.onChange?.(valueNum)
+	}
+
+	const handleNumberChange = (newValue: string) => {
+		const valueNum = isInt ? parseInt(newValue) : parseFloat(newValue)
+		props.onNumberChange?.(valueNum)
 	}
 
 	// RENDER
-	return <TextInput {...props} 
-		onChange={handleChange}
+	return <TextInput {...props}
+		onChange={handleNumberChange}
 	/>
 }
 
 export default NumberInput
 
-function format(value: string, decimals?: number, min?: number, max?: number): string {
+function format(value: string, decimals?: number, min?: number, max?: number, isInt?: boolean): string {
 	value = value.replace(/[^0-9.,+-]/g, "")
 	if (decimals != null && (value.includes(".") || value.includes(","))) {
 		value = parseFloat(value).toString()//.toFixed(decimals)
@@ -44,7 +51,7 @@ function format(value: string, decimals?: number, min?: number, max?: number): s
 			value = value.slice(0, index + decimals + 1)
 		}
 	}
-	let valueNum = parseFloat(value)
+	let valueNum = isInt ? parseInt(value) : parseFloat(value)
 	if (min != null && valueNum < min) value = min.toString()
 	if (max != null && valueNum > max) value = max.toString()
 	return value
