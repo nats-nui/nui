182d1d57d8173960e30a6d1faa6d39c04fc2546f
ivano.iorio <ivano.iorio@zerofarms.it>
2024-03-02 18:17:40 +0100

    improve components

--- frontend/src/components/stacks/streams/detail/EditForm.tsx
+++ frontend/src/components/stacks/streams/detail/EditForm.tsx
@@ -12,11 +12,12 @@ import EditStringRow from "@/components/rows/EditStringRow"
 import layoutSo, { COLOR_VAR } from "@/stores/layout"
 import { StreamStore } from "@/stores/stacks/streams/detail"
 import { EDIT_STATE } from "@/types"
-import { DISCARD, RETENTION, STORAGE, Source, StreamConfig } from "@/types/Stream"
+import { DISCARD, RETENTION, STORAGE, StreamConfig } from "@/types/Stream"
 import { useStore } from "@priolo/jon"
-import { FunctionComponent, useRef, useState } from "react"
+import { FunctionComponent } from "react"
 import ListDialog from "../../../dialogs/ListDialog"
 import SourcesCmp from "./source/SourcesCmp"
+import TextArea from "@/components/input/TextArea"
 
 
 
@@ -75,8 +76,10 @@ const EditForm: FunctionComponent<Props> = ({
 
 	return <Form>
 
+
+		<div className="lbl-prop-title">BASIC INFO</div>
 		<BoxV>
-			<Label>NAME</Label>
+			<div className="lbl-prop">NAME</div>
 			<TextInput
 				value={config.name}
 				onChange={name => handlePropChange({ name })}
@@ -85,8 +88,8 @@ const EditForm: FunctionComponent<Props> = ({
 		</BoxV>
 
 		<BoxV>
-			<Label>DESCRIPTION</Label>
-			<TextInput
+			<div className="lbl-prop">DESCRIPTION</div>
+			<TextArea
 				value={config.description}
 				onChange={description => handlePropChange({ description })}
 				readOnly={inRead}
@@ -94,7 +97,19 @@ const EditForm: FunctionComponent<Props> = ({
 		</BoxV>
 
 		<BoxV>
-			<Label>SUBJECTS</Label>
+			<div className="lbl-prop">STORAGE</div>
+			<ListDialog width={80}
+				store={streamSo}
+				select={Object.values(STORAGE).indexOf(config.storage ?? STORAGE.FILE)}
+				items={Object.values(STORAGE)}
+				RenderRow={({ item }) => item.toUpperCase()}
+				readOnly={inRead || !inNew}
+				onSelect={index => handlePropChange({ storage: Object.values(STORAGE)[index] })}
+			/>
+		</BoxV>
+
+		<BoxV>
+			<div className="lbl-prop">FAVORITE SUBS</div>
 			<EditList<string>
 				items={config.subjects}
 				onItemsChange={subjects => handlePropChange({ subjects })}
@@ -106,8 +121,18 @@ const EditForm: FunctionComponent<Props> = ({
 		</BoxV>
 
 		<BoxV>
-			<Label>RETENTION</Label>
-			<ListDialog
+			<div className="lbl-prop">SOURCES</div>
+			<SourcesCmp store={streamSo} />
+		</BoxV>
+
+
+
+
+
+		<div className="lbl-prop-title">RETENTION</div>
+		<BoxV>
+			<div className="lbl-prop">POLICY</div>
+			<ListDialog width={80}
 				store={streamSo}
 				select={Object.values(RETENTION).indexOf(config.retention ?? RETENTION.INTEREST)}
 				items={Object.values(RETENTION)}
@@ -116,40 +141,9 @@ const EditForm: FunctionComponent<Props> = ({
 				onSelect={index => handlePropChange({ retention: Object.values(RETENTION)[index] })}
 			/>
 		</BoxV>
-
 		<BoxV>
-			<Label>MAX CONSUMERS</Label>
-			<NumberInput
-				style={{ flex: 1 }}
-				value={config.maxConsumers}
-				onChange={maxConsumers => handlePropChange({ maxConsumers })}
-				readOnly={inRead || !inNew}
-			/>
-		</BoxV>
-
-		<BoxV>
-			<Label>MAX MESSAGES</Label>
-			<NumberInput
-				style={{ flex: 1 }}
-				value={config.maxMsgs}
-				onChange={maxMsgs => handlePropChange({ maxMsgs })}
-				readOnly={inRead}
-			/>
-		</BoxV>
-
-		<BoxV>
-			<Label>MAX BYTES</Label>
-			<NumberInput
-				style={{ flex: 1 }}
-				value={config.maxBytes}
-				onChange={maxBytes => handlePropChange({ maxBytes })}
-				readOnly={inRead}
-			/>
-		</BoxV>
-
-		<BoxV>
-			<Label>DISCARD</Label>
-			<ListDialog
+			<div className="lbl-prop">DISCARD</div>
+			<ListDialog width={80}
 				store={streamSo}
 				select={Object.values(DISCARD).indexOf(config.discard ?? DISCARD.OLD)}
 				items={Object.values(DISCARD)}
@@ -161,9 +155,38 @@ const EditForm: FunctionComponent<Props> = ({
 				}}
 			/>
 		</BoxV>
+		<Box>
+			<IconToggle
+				check={config.allowRollupHdrs}
+				onChange={allowRollupHdrs => handlePropChange({ allowRollupHdrs })}
+				readOnly={inRead}
+			/>
+			<div className="lbl-prop">ALLOW ROLL UP HDRS</div>
+		</Box>
+		<Box>
+			<IconToggle
+				check={config.denyDelete}
+				onChange={denyDelete => handlePropChange({ denyDelete })}
+				readOnly={inRead || !inNew}
+			/>
+			<div className="lbl-prop">DENY DELETE</div>
+		</Box>
+		<Box>
+			<IconToggle
+				check={config.denyPurge}
+				onChange={denyPurge => handlePropChange({ denyPurge })}
+				readOnly={inRead || !inNew}
+			/>
+			<div className="lbl-prop">DENY PURGE</div>
+		</Box>
 
+
+
+
+
+		<div className="lbl-prop-title">LIMIT</div>
 		<BoxV>
-			<Label>MAX AGE</Label>
+			<div className="lbl-prop">MAX AGE</div>
 			<NumberInput
 				style={{ flex: 1 }}
 				value={config.maxAge}
@@ -171,9 +194,44 @@ const EditForm: FunctionComponent<Props> = ({
 				readOnly={inRead}
 			/>
 		</BoxV>
-
 		<BoxV>
-			<Label>MAX MSGS PER SUBJECT</Label>
+			<div className="lbl-prop">MAX BYTES</div>
+			<NumberInput
+				style={{ flex: 1 }}
+				value={config.maxBytes}
+				onChange={maxBytes => handlePropChange({ maxBytes })}
+				readOnly={inRead}
+			/>
+		</BoxV>
+		<BoxV>
+			<div className="lbl-prop">MAX CONSUMERS</div>
+			<NumberInput
+				style={{ flex: 1 }}
+				value={config.maxConsumers}
+				onChange={maxConsumers => handlePropChange({ maxConsumers })}
+				readOnly={inRead || !inNew}
+			/>
+		</BoxV>
+		<BoxV>
+			<div className="lbl-prop">MAX MSG SIZE</div>
+			<NumberInput
+				style={{ flex: 1 }}
+				value={config.maxMsgSize}
+				onChange={maxMsgSize => handlePropChange({ maxMsgSize })}
+				readOnly={inRead}
+			/>
+		</BoxV>
+		<BoxV>
+			<div className="lbl-prop">MAX MESSAGES</div>
+			<NumberInput
+				style={{ flex: 1 }}
+				value={config.maxMsgs}
+				onChange={maxMsgs => handlePropChange({ maxMsgs })}
+				readOnly={inRead}
+			/>
+		</BoxV>
+		<BoxV>
+			<div className="lbl-prop">MAX MSGS PER SUBJECT</div>
 			<NumberInput
 				style={{ flex: 1 }}
 				value={config.maxMsgsPerSubject}
@@ -182,30 +240,14 @@ const EditForm: FunctionComponent<Props> = ({
 			/>
 		</BoxV>
 
-		<BoxV>
-			<Label>MAX MSG SIZE</Label>
-			<NumberInput
-				style={{ flex: 1 }}
-				value={config.maxMsgSize}
-				onChange={maxMsgSize => handlePropChange({ maxMsgSize })}
-				readOnly={inRead}
-			/>
-		</BoxV>
 
-		<BoxV>
-			<Label>STORAGE</Label>
-			<ListDialog
-				store={streamSo}
-				select={Object.values(STORAGE).indexOf(config.storage ?? STORAGE.FILE)}
-				items={Object.values(STORAGE)}
-				RenderRow={({ item }) => item}
-				readOnly={inRead || !inNew}
-				onSelect={index => handlePropChange({ storage: Object.values(STORAGE)[index] })}
-			/>
-		</BoxV>
 
+
+		
+
+		<div className="lbl-prop-title">PLACMENT</div>
 		<BoxV>
-			<Label>NUM REPLICAS</Label>
+			<div className="lbl-prop">NUM REPLICAS</div>
 			<NumberInput
 				style={{ flex: 1 }}
 				value={config.numReplicas}
@@ -213,37 +255,8 @@ const EditForm: FunctionComponent<Props> = ({
 				readOnly={inRead}
 			/>
 		</BoxV>
-
-		<Box>
-			<IconToggle
-				check={config.noAck}
-				onChange={noAck => handlePropChange({ noAck })}
-				readOnly={inRead}
-			/>
-			<Label>NO ACK</Label>
-		</Box>
-
 		<BoxV>
-			<Label>TEMPLATE OWNER</Label>
-			<TextInput
-				value={config.templateOwner}
-				onChange={templateOwner => handlePropChange({ templateOwner })}
-				readOnly={inRead}
-			/>
-		</BoxV>
-
-		<BoxV>
-			<Label>DUPLICATE WINDOW</Label>
-			<NumberInput
-				style={{ flex: 1 }}
-				value={config.duplicateWindow}
-				onChange={duplicateWindow => handlePropChange({ duplicateWindow })}
-				readOnly={inRead}
-			/>
-		</BoxV>
-
-		<BoxV>
-			<Label>PLACEMENT</Label>
+			<div className="lbl-prop">PLACEMENT</div>
 			<Quote>
 				<BoxV>
 					<Label type={LABELS.SUBTEXT}>NAME</Label>
@@ -258,6 +271,36 @@ const EditForm: FunctionComponent<Props> = ({
 			</Quote>
 		</BoxV>
 
+
+
+
+
+		<div className="lbl-prop-title">!!!DA CAPIRE!!!</div>
+		<Box>
+			<IconToggle
+				check={config.noAck}
+				onChange={noAck => handlePropChange({ noAck })}
+				readOnly={inRead}
+			/>
+			<div className="lbl-prop">NO ACK</div>
+		</Box>
+		<BoxV>
+			<div className="lbl-prop">TEMPLATE OWNER</div>
+			<TextInput
+				value={config.templateOwner}
+				onChange={templateOwner => handlePropChange({ templateOwner })}
+				readOnly={inRead}
+			/>
+		</BoxV>
+		<BoxV>
+			<div className="lbl-prop">DUPLICATE WINDOW</div>
+			<NumberInput
+				style={{ flex: 1 }}
+				value={config.duplicateWindow}
+				onChange={duplicateWindow => handlePropChange({ duplicateWindow })}
+				readOnly={inRead}
+			/>
+		</BoxV>
 		<BoxV>
 			<Box>
 				<IconToggle
@@ -265,7 +308,7 @@ const EditForm: FunctionComponent<Props> = ({
 					onChange={handleMirrorCheck}
 					readOnly={inRead || !inNew}
 				/>
-				<Label type={LABELS.TEXT}>MIRROR</Label>
+				<div className="lbl-prop">MIRROR</div>
 			</Box>
 			<Accordion open={!!config.mirror}>
 				<Quote>
@@ -309,45 +352,14 @@ const EditForm: FunctionComponent<Props> = ({
 				</Quote>
 			</Accordion>
 		</BoxV>
-
-		<SourcesCmp store={streamSo} />
-		
 		<Box>
 			<IconToggle
 				check={config.sealed}
 				onChange={sealed => handlePropChange({ sealed })}
 				readOnly={inRead}
 			/>
-			<Label>SEALED</Label>
+			<div className="lbl-prop">SEALED</div>
 		</Box>
-
-		<Box>
-			<IconToggle
-				check={config.denyDelete}
-				onChange={denyDelete => handlePropChange({ denyDelete })}
-				readOnly={inRead || !inNew}
-			/>
-			<Label>DENY DELETE</Label>
-		</Box>
-
-		<Box>
-			<IconToggle
-				check={config.denyPurge}
-				onChange={denyPurge => handlePropChange({ denyPurge })}
-				readOnly={inRead || !inNew}
-			/>
-			<Label>DENY PURGE</Label>
-		</Box>
-
-		<Box>
-			<IconToggle
-				check={config.allowRollupHdrs}
-				onChange={allowRollupHdrs => handlePropChange({ allowRollupHdrs })}
-				readOnly={inRead}
-			/>
-			<Label>ALLOW ROLL UP HDRS</Label>
-		</Box>
-
 		<BoxV>
 			<Box>
 				<IconToggle
@@ -371,7 +383,7 @@ const EditForm: FunctionComponent<Props> = ({
 					}}
 					readOnly={inRead}
 				/>
-				<Label>REPUBLISH</Label>
+				<div className="lbl-prop">REPUBLISH</div>
 			</Box>
 			<Accordion open={!!config.republish}>
 				<Quote>
