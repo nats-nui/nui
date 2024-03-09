2042664ae260fd0d0b39fbee853fb583812869c3
pricelessrabbit <pricelessrabbit@gmail.com>
2024-03-09 19:08:27 +0100

    added additional info to consumer form show

--- frontend/src/components/stacks/consumers/detail/ShowForm.tsx
+++ frontend/src/components/stacks/consumers/detail/ShowForm.tsx
@@ -29,34 +29,125 @@ const ShowForm: FunctionComponent<Props> = ({
 
 	return <Form>
 
+		<div className="lbl-prop-title">CONSUMER</div>
+
 		<BoxV>
-			<Label>NAME</Label>
-			<TextInput readOnly
-				value={consumer.name}
-			/>
+			<div className="lbl-prop">NAME</div>
+			<Label>{consumer.name || "-"}</Label>
 		</BoxV>
 
 		<BoxV>
-			<Label>DESCRIPTION</Label>
-			<TextInput readOnly
-				value={consumer.config.description}
-			/>
+			<div className="lbl-prop">DESCRIPTION</div>
+			<Label>{consumer.description || "-"}</Label>
 		</BoxV>
 
 		<BoxV>
-			<Label>DELIVERY POLICY</Label>
-			<TextInput readOnly
-				value={consumer.config.deliverPolicy}
-			/>
+			<div className="lbl-prop">DURABLE NAME</div>
+			<Label>{consumer.durableName || "-"}</Label>
 		</BoxV>
 
 		<BoxV>
-			<Label>MAX DELIVER</Label>
-			<TextInput readOnly
-				value={consumer.config.maxDeliver}
-			/>
+			<div className="lbl-prop">CREATION DATETIME</div>
+			<Label>{consumer.created || "-"}</Label>
 		</BoxV>
 
+		<div className="lbl-prop-title">MESSAGES</div>
+
+		<BoxV>
+			<div className="lbl-prop">WAITING COUNT</div>
+			<Label>{consumer.numWaiting || "-"}</Label>
+		</BoxV>
+
+		<BoxV>
+			<div className="lbl-prop">PENDING COUNT</div>
+			<Label>{consumer.numPending || "-"}</Label>
+		</BoxV>
+
+		<BoxV>
+			<div className="lbl-prop">ACKS PENDING COUNT</div>
+			<Label>{consumer.numAckPending || "-"}</Label>
+		</BoxV>
+
+		<BoxV>
+			<div className="lbl-prop">REDELIVERED COUNT</div>
+			<Label>{consumer.numRedelivered || "-"}</Label>
+		</BoxV>
+
+		<div className="lbl-prop-title">LAST DELIVERED</div>
+
+		<BoxV>
+			<div className="lbl-prop">DATETIME</div>
+			<Label>{consumer.delivered?.lastActive || "-"}</Label>
+		</BoxV>
+
+		<BoxV>
+			<div className="lbl-prop">STREAM SEQ</div>
+			<Label>{consumer.delivered?.stream || "-"}</Label>
+		</BoxV>
+
+		<BoxV>
+			<div className="lbl-prop">CONSUMER SEQ</div>
+			<Label>{consumer.delivered?.consumer || "-"}</Label>
+		</BoxV>
+
+		<div className="lbl-prop-title">LAST ACKED</div>
+
+		<BoxV>
+			<div className="lbl-prop">DATETIME</div>
+			<Label>{consumer.ackFloor?.lastActive || "-"}</Label>
+		</BoxV>
+
+		<BoxV>
+			<div className="lbl-prop">STREAM SEQ</div>
+			<Label>{consumer.ackFloor?.stream || "-"}</Label>
+		</BoxV>
+
+		<BoxV>
+			<div className="lbl-prop">CONSUMER SEQ</div>
+			<Label>{consumer.ackFloor?.consumer || "-"}</Label>
+		</BoxV>
+
+		<div className="lbl-prop-title">CONFIG</div>
+
+		<BoxV>
+			<div className="lbl-prop">DELIVER POLICY</div>
+			<Label>{consumer.config.deliverPolicy || "-"}</Label>
+		</BoxV>
+
+		<BoxV>
+			<div className="lbl-prop">MAX DELIVER</div>
+			<Label>{consumer.config.maxDeliver || "-"}</Label>
+		</BoxV>
+
+		<BoxV>
+			<div className="lbl-prop">OPT START SEQ</div>
+			<Label>{consumer.config.optStartSeq || "-"}</Label>
+		</BoxV>
+
+		<BoxV>
+			<div className="lbl-prop">OPT START TIME</div>
+			<Label>{consumer.config.optStartTime || "-"}</Label>
+		</BoxV>
+
+		<BoxV>
+			<div className="lbl-prop">ACK POLICY</div>
+			<Label>{consumer.config.ackPolicy || "-"}</Label>
+		</BoxV>
+
+		<BoxV>
+			<div className="lbl-prop">ACK WAIT</div>
+			<Label>{consumer.config.ackWait || "-"}</Label>
+		</BoxV>
+
+		<BoxV>
+			<div className="lbl-prop">MAX WAITING</div>
+			<Label>{consumer.config.maxWaiting || "-"}</Label>
+		</BoxV>
+
+		<BoxV>
+			<div className="lbl-prop">MAX ACK PENDING</div>
+			<Label>{consumer.config.maxAckPending || "-"}</Label>
+		</BoxV>
 
 	</Form>
 }
