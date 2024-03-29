e72aad418007a3e4f66b38fe55828a52a2a72324
priolo <iorioivano@gmail.com>
2024-03-29 08:35:03 +0100

    add func compress expande for cards parents

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,18 @@
-import srcBg from "@/assets/bg4.jpg"
 import docSo, { DocState } from "@/stores/docs"
-import layoutSo, { LayoutState } from "@/stores/layout"
 import { ViewStore } from "@/stores/stacks/viewBase"
 import { useStore } from "@priolo/jon"
 import { FunctionComponent, useMemo } from "react"
-import CardCmp from "./components/cards/CardCmp"
+import cls from "./App.module.css"
 import DragCmp from "./components/DragCmp"
 import TooltipCmp from "./components/TooltipCmp"
+import CardCmp from "./components/cards/CardCmp"
 import MainMenu from "./components/mainMenu/MainMenu"
-import { Theme } from "./stores/layout/utils"
 
 
 
 const App: FunctionComponent = () => {
 
 	// STORES
-	const layoutSa: LayoutState = useStore(layoutSo)
 	const docSa: DocState = useStore(docSo)
 
 	// HOOKS
@@ -28,13 +25,13 @@ const App: FunctionComponent = () => {
 
 	// RENDER
 	return (
-		<div style={cssApp(layoutSa.theme)}>
+		<div className={cls.root}>
 
 			<MainMenu />
 
-			<div style={cssContent}>
+			<div className={cls.content}>
 
-				<div style={cssFixed}>
+				<div className={cls.fixed}>
 					{storesAnchored.map((store: ViewStore) =>
 						<CardCmp key={store.state.uuid}
 							store={store}
@@ -43,7 +40,7 @@ const App: FunctionComponent = () => {
 					{/* <DropArea index={-1} /> */}
 				</div>
 
-				<div style={cssVisible}>
+				<div className={cls.visible}>
 					{stores.map((store: ViewStore) =>
 						<CardCmp key={store.state.uuid}
 							store={store}
@@ -60,39 +57,3 @@ const App: FunctionComponent = () => {
 }
 
 export default App
-
-const cssApp = (theme: Theme): React.CSSProperties => ({
-	position: "relative",
-	height: "100%",
-	display: "flex",
-
-	color: theme.palette.default.fg,
-	//backgroundColor: "#333333",
-
-	backgroundImage: `url("${srcBg}")`,
-	backgroundColor: "rgba(0, 0, 0, 0.8)",
-	backgroundBlendMode: "darken",
-	// backgroundSize: "100% 100%",
-	//backgroundRepeat: "repeat-x",
-})
-
-const cssContent: React.CSSProperties = {
-	flex: 1,
-	display: "flex",
-	overflowX: "auto",
-}
-
-const cssFixed: React.CSSProperties = {
-	zIndex: 1,
-	display: "flex",
-	position: "sticky",
-	left: 0, right: 0,
-	backgroundColor: "#636363",
-	padding: "10px 25px 10px 0px",
-	//borderRight: "2px dashed white",
-}
-
-const cssVisible: React.CSSProperties = {
-	zIndex: 0, display: "flex", flex: 1,
-	padding: "10px 0px 10px 0px",
-}
