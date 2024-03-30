8681939cdfb3b80dfc9e71d8db41c924934f79e2
ivano.iorio <ivano.iorio@zerofarms.it>
2024-03-30 20:10:32 +0100

    drawer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,11 @@ import { ViewStore } from "@/stores/stacks/viewBase"
 import { useStore } from "@priolo/jon"
 import { FunctionComponent, useMemo, useState } from "react"
 import cls from "./App.module.css"
+import DrawerCmp from "./DrawerCmp"
 import DragCmp from "./components/DragCmp"
 import TooltipCmp from "./components/TooltipCmp"
 import CardCmp from "./components/cards/CardCmp"
 import MainMenu from "./components/mainMenu/MainMenu"
-import IconButton from "./components/buttons/IconButton"
-import ExpandHIcon from "./icons/ExpandHIcon"
-import CompressHIcon from "./icons/CompressHIcon"
 
 
 
@@ -20,10 +18,7 @@ const App: FunctionComponent = () => {
 	const docSa: DocState = useStore(docSo)
 
 	// HOOKS
-	const [storesAnchored, stores] = useMemo(() => [
-		docSo.getAnchored(),
-		docSo.getVisible(),
-	], [docSa.all, docSa.anchored])
+	const stores = useMemo(() => docSo.getVisible(), [docSa.all, docSa.anchored])
 
 	const [exp, setExp] = useState(true)
 
@@ -45,35 +40,12 @@ const App: FunctionComponent = () => {
 					)}
 				</div>
 
-				<div className={cls.fix} >
+				<DrawerCmp />
 
-					<div style={{ display: "flex", flexDirection: "column", gap: 10}}>
-						<IconButton
-							style={{
-								//color: "var(--var-1)", backgroundColor:"var(--text-bg)",
-								color: "var(--text-bg)", backgroundColor: "var(--var-1)",
-								//alignSelf: "start"
-							}}
-							onClick={() => setExp(!exp)}
-						>{exp ? <ExpandHIcon /> : <CompressHIcon />}</IconButton>
-						<div className="bars-alert-bg-1" style={{ flex: 1}} />
-						<div className={cls.fix_label}>DRAWER</div>
-						<div className="bars-alert-bg-1" style={{ flex: 1}} />
-					</div>
-
-					<div style={cssFixed(exp)}>
-						{storesAnchored.map((store: ViewStore) =>
-							<CardCmp key={store.state.uuid}
-								store={store}
-							/>
-						)}
-					</div>
-
-					{/* <DropArea index={-1} /> */}
-				</div>
 			</div>
 
 			<DragCmp />
+
 			<TooltipCmp />
 
 		</div>
@@ -83,5 +55,7 @@ const App: FunctionComponent = () => {
 export default App
 
 const cssFixed = (exp: boolean): React.CSSProperties => ({
-	display: exp ? "flex" : "none",
+	display: "flex",
+	width: exp ? 500 : 0,
+	transition: "width 300ms",
 })
