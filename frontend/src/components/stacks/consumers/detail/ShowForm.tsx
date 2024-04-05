db01bdc586b88119b6511357874d93e3b228d82b
ivano.iorio <ivano.iorio@zerofarms.it>
2024-04-05 20:44:47 +0200

    improva accordion

--- frontend/src/components/stacks/consumers/detail/ShowForm.tsx
+++ frontend/src/components/stacks/consumers/detail/ShowForm.tsx
@@ -1,3 +1,4 @@
+import TitleAccordion from "@/components/accordion/TitleAccordion"
 import { ConsumerStore } from "@/stores/stacks/consumer/detail"
 import { dateShow } from "@/utils/time"
 import { useStore } from "@priolo/jon"
@@ -26,125 +27,135 @@ const ShowForm: FunctionComponent<Props> = ({
 
 	return <div className="lyt-form">
 
-		<div className="lbl-prop-title">CONSUMER</div>
+		<TitleAccordion title="CONSUMER">
 
-		<div className="lyt-v">
-			<div className="lbl-prop">NAME</div>
-			<div className="lbl-input-readonly">{consumer.name ?? "-"}</div>
-		</div>
+			<div className="lyt-v">
+				<div className="lbl-prop">NAME</div>
+				<div className="lbl-input-readonly">{consumer.name ?? "-"}</div>
+			</div>
 
-		<div className="lyt-v">
-			<div className="lbl-prop">DESCRIPTION</div>
-			<div className="lbl-input-readonly">{consumer.config.description ?? "-"}</div>
-		</div>
+			<div className="lyt-v">
+				<div className="lbl-prop">DESCRIPTION</div>
+				<div className="lbl-input-readonly">{consumer.config.description ?? "-"}</div>
+			</div>
 
-		<div className="lyt-v">
-			<div className="lbl-prop">DURABLE NAME</div>
-			<div className="lbl-input-readonly">{consumer.config.durableName ?? "-"}</div>
-		</div>
+			<div className="lyt-v">
+				<div className="lbl-prop">DURABLE NAME</div>
+				<div className="lbl-input-readonly">{consumer.config.durableName ?? "-"}</div>
+			</div>
 
-		<div className="lyt-v">
-			<div className="lbl-prop">CREATION DATETIME</div>
-			<div className="lbl-input-readonly">{dateShow(consumer.created)}</div>
-		</div>
+			<div className="lyt-v">
+				<div className="lbl-prop">CREATION DATETIME</div>
+				<div className="lbl-input-readonly">{dateShow(consumer.created)}</div>
+			</div>
 
-		<div className="lbl-prop-title">MESSAGES</div>
+		</TitleAccordion>
 
-		<div className="lyt-v">
-			<div className="lbl-prop">WAITING COUNT</div>
-			<div className="lbl-input-readonly">{consumer.numWaiting ?? "-"}</div>
-		</div>
+		<TitleAccordion title="MESSAGES">
 
-		<div className="lyt-v">
-			<div className="lbl-prop">PENDING COUNT</div>
-			<div className="lbl-input-readonly">{consumer.numPending ?? "-"}</div>
-		</div>
+			<div className="lyt-v">
+				<div className="lbl-prop">WAITING COUNT</div>
+				<div className="lbl-input-readonly">{consumer.numWaiting ?? "-"}</div>
+			</div>
 
-		<div className="lyt-v">
-			<div className="lbl-prop">ACKS PENDING COUNT</div>
-			<div className="lbl-input-readonly">{consumer.numAckPending ?? "-"}</div>
-		</div>
+			<div className="lyt-v">
+				<div className="lbl-prop">PENDING COUNT</div>
+				<div className="lbl-input-readonly">{consumer.numPending ?? "-"}</div>
+			</div>
 
-		<div className="lyt-v">
-			<div className="lbl-prop">REDELIVERED COUNT</div>
-			<div className="lbl-input-readonly">{consumer.numRedelivered ?? "-"}</div>
-		</div>
+			<div className="lyt-v">
+				<div className="lbl-prop">ACKS PENDING COUNT</div>
+				<div className="lbl-input-readonly">{consumer.numAckPending ?? "-"}</div>
+			</div>
 
-		<div className="lbl-prop-title">LAST DELIVERED</div>
+			<div className="lyt-v">
+				<div className="lbl-prop">REDELIVERED COUNT</div>
+				<div className="lbl-input-readonly">{consumer.numRedelivered ?? "-"}</div>
+			</div>
 
-		<div className="lyt-v">
-			<div className="lbl-prop">DATETIME</div>
-			<div className="lbl-input-readonly">{consumer.delivered?.lastActive ?? "-"}</div>
-		</div>
+		</TitleAccordion>
 
-		<div className="lyt-v">
-			<div className="lbl-prop">STREAM SEQ</div>
-			<div className="lbl-input-readonly">{consumer.delivered?.streamSeq ?? "-"}</div>
-		</div>
+		<TitleAccordion title="LAST DELIVERED">
 
-		<div className="lyt-v">
-			<div className="lbl-prop">CONSUMER SEQ</div>
-			<div className="lbl-input-readonly">{consumer.delivered?.consumerSeq ?? "-"}</div>
-		</div>
+			<div className="lyt-v">
+				<div className="lbl-prop">DATETIME</div>
+				<div className="lbl-input-readonly">{consumer.delivered?.lastActive ?? "-"}</div>
+			</div>
 
-		<div className="lbl-prop-title">LAST ACKED</div>
+			<div className="lyt-v">
+				<div className="lbl-prop">STREAM SEQ</div>
+				<div className="lbl-input-readonly">{consumer.delivered?.streamSeq ?? "-"}</div>
+			</div>
 
-		<div className="lyt-v">
-			<div className="lbl-prop">DATETIME</div>
-			<div className="lbl-input-readonly">{consumer.ackFloor?.lastActive ?? "-"}</div>
-		</div>
+			<div className="lyt-v">
+				<div className="lbl-prop">CONSUMER SEQ</div>
+				<div className="lbl-input-readonly">{consumer.delivered?.consumerSeq ?? "-"}</div>
+			</div>
 
-		<div className="lyt-v">
-			<div className="lbl-prop">STREAM SEQ</div>
-			<div className="lbl-input-readonly">{consumer.ackFloor?.streamSeq ?? "-"}</div>
-		</div>
+		</TitleAccordion>
 
-		<div className="lyt-v">
-			<div className="lbl-prop">CONSUMER SEQ</div>
-			<div className="lbl-input-readonly">{consumer.ackFloor?.consumerSeq ?? "-"}</div>
-		</div>
+		<TitleAccordion title="LAST ACKED">
 
-		<div className="lbl-prop-title">CONFIG</div>
+			<div className="lyt-v">
+				<div className="lbl-prop">DATETIME</div>
+				<div className="lbl-input-readonly">{consumer.ackFloor?.lastActive ?? "-"}</div>
+			</div>
 
-		<div className="lyt-v">
-			<div className="lbl-prop">DELIVER POLICY</div>
-			<div className="lbl-input-readonly">{consumer.config.deliverPolicy ?? "-"}</div>
-		</div>
+			<div className="lyt-v">
+				<div className="lbl-prop">STREAM SEQ</div>
+				<div className="lbl-input-readonly">{consumer.ackFloor?.streamSeq ?? "-"}</div>
+			</div>
 
-		<div className="lyt-v">
-			<div className="lbl-prop">MAX DELIVER</div>
-			<div className="lbl-input-readonly">{consumer.config.maxDeliver ?? "-"}</div>
-		</div>
+			<div className="lyt-v">
+				<div className="lbl-prop">CONSUMER SEQ</div>
+				<div className="lbl-input-readonly">{consumer.ackFloor?.consumerSeq ?? "-"}</div>
+			</div>
 
-		<div className="lyt-v">
-			<div className="lbl-prop">OPT START SEQ</div>
-			<div className="lbl-input-readonly">{consumer.config.optStartSeq ?? "-"}</div>
-		</div>
+		</TitleAccordion>
 
-		<div className="lyt-v">
-			<div className="lbl-prop">OPT START TIME</div>
-			<div className="lbl-input-readonly">{consumer.config.optStartTime ?? "-"}</div>
-		</div>
+		<TitleAccordion title="CONFIG">
 
-		<div className="lyt-v">
-			<div className="lbl-prop">ACK POLICY</div>
-			<div className="lbl-input-readonly">{consumer.config.ackPolicy ?? "-"}</div>
-		</div>
+			<div className="lyt-v">
+				<div className="lbl-prop">DELIVER POLICY</div>
+				<div className="lbl-input-readonly">{consumer.config.deliverPolicy ?? "-"}</div>
+			</div>
 
-		<div className="lyt-v">
-			<div className="lbl-prop">ACK WAIT</div>
-			<div className="lbl-input-readonly">{consumer.config.ackWait ?? "-"}</div>
-		</div>
+			<div className="lyt-v">
+				<div className="lbl-prop">MAX DELIVER</div>
+				<div className="lbl-input-readonly">{consumer.config.maxDeliver ?? "-"}</div>
+			</div>
 
-		<div className="lyt-v">
-			<div className="lbl-prop">MAX WAITING</div>
-			<div className="lbl-input-readonly">{consumer.config.maxWaiting ?? "-"}</div>
-		</div>
+			<div className="lyt-v">
+				<div className="lbl-prop">OPT START SEQ</div>
+				<div className="lbl-input-readonly">{consumer.config.optStartSeq ?? "-"}</div>
+			</div>
 
-		<div className="lyt-v">
-			<div className="lbl-prop">MAX ACK PENDING</div>
-			<div className="lbl-input-readonly">{consumer.config.maxAckPending ?? "-"}</div>
-		</div>
+			<div className="lyt-v">
+				<div className="lbl-prop">OPT START TIME</div>
+				<div className="lbl-input-readonly">{consumer.config.optStartTime ?? "-"}</div>
+			</div>
+
+			<div className="lyt-v">
+				<div className="lbl-prop">ACK POLICY</div>
+				<div className="lbl-input-readonly">{consumer.config.ackPolicy ?? "-"}</div>
+			</div>
+
+			<div className="lyt-v">
+				<div className="lbl-prop">ACK WAIT</div>
+				<div className="lbl-input-readonly">{consumer.config.ackWait ?? "-"}</div>
+			</div>
+
+			<div className="lyt-v">
+				<div className="lbl-prop">MAX WAITING</div>
+				<div className="lbl-input-readonly">{consumer.config.maxWaiting ?? "-"}</div>
+			</div>
+
+			<div className="lyt-v">
+				<div className="lbl-prop">MAX ACK PENDING</div>
+				<div className="lbl-input-readonly">{consumer.config.maxAckPending ?? "-"}</div>
+			</div>
+
+		</TitleAccordion>
 
 	</div>
 }
