1c6b1a08ee46b0b9c16e2c2d3502e9a41f8704f6
priolo <iorioivano@gmail.com>
2024-04-27 13:56:30 +0200

    improved connections list status icons

---
+++ frontend/src/icons/AlertIcon.tsx
@@ -0,0 +1,13 @@
+const AlertIcon = (props) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="14" height="14" viewBox="0 0 14 14"
+		fill="currentColor" stroke="none"
+		{...props}
+	>
+		<rect x="6" y="2" width="2" height="6" />
+		<rect x="6" y="10" width="2" height="2" />
+	</svg>
+)
+
+export default AlertIcon
