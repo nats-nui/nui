57df7bbd3c31f2bcbf11abd3e84e899d1f179941
pricelessrabbit <pricelessrabbit@gmail.com>
2024-05-14 21:08:04 +0200

    updated client consumer defintion and client api

--- frontend/src/types/Consumer.ts
+++ frontend/src/types/Consumer.ts
@@ -28,33 +28,53 @@ export interface StreamConsumer {
 }
 
 export interface ConsumerConfig {
-	durableName?: string; // The name of the durable subscription
-    name?: string; // The name of the consumer
-    description?: string; // A description for the consumer
-    deliverPolicy: number; // The policy for message delivery
-    optStartSeq?: number; // The sequence number to start delivering messages from
-    optStartTime?: any; // The time to start delivering messages from
-    ackPolicy: number; // The policy for message acknowledgement
-    ackWait?: number; // The time to wait for message acknowledgement
-    maxDeliver?: number; // The maximum number of times a message will be delivered
-    backoff?: number[]; // The backoff strategy for redelivery
-    filterSubject?: string; // The subject to filter on for message delivery
-    replayPolicy: number; // The policy for message replay
-    rateLimitBps?: number; // The rate limit in bytes per second for message delivery
-    sampleFreq?: string; // The sampling frequency for metrics
-    maxWaiting?: number; // The maximum number of waiting messages
-    maxAckPending?: number; // The maximum number of unacknowledged messages
-    flowControl?: boolean; // Whether flow control is enabled
-    idleHeartbeat?: number; // The idle heartbeat interval
-    headersOnly?: boolean; // Whether to deliver only the headers of the message
-    maxBatch?: number; // The maximum batch size for message delivery
-    maxExpires?: number; // The maximum time a message can exist
-    maxBytes?: number; // The maximum size of a message in bytes
-    deliverSubject?: string; // The subject to deliver the messages to
-    deliverGroup?: string; // The group to deliver the messages to
-    inactiveThreshold?: number; // The threshold for marking a consumer as inactive
-    numReplicas: number; // The number of replicas for the consumer
-    memStorage?: boolean; // Whether to use memory storage
+	durableName?: string; // The name of the durable subscription - Not editable
+	name?: string; // The name of the consumer - Not editable
+	description?: string; // A description for the consumer - Editable
+	deliverPolicy: DeliverPolicy; // The policy for message delivery - Not editable
+	optStartSeq?: number; // The sequence number to start delivering messages from - Not editable
+	optStartTime?: any; // The time to start delivering messages from - Not editable
+	ackPolicy: AckPolicy; // The policy for message acknowledgement - Not editable
+	ackWait?: number; // The time to wait for message acknowledgement - Editable
+	maxDeliver?: number; // The maximum number of times a message will be delivered - Editable
+	backoff?: number[]; // The backoff strategy for redelivery - Editable
+	filterSubject?: string; // The subject to filter on for message delivery - Not editable
+	replayPolicy: ReplayPolicy; // The policy for message replay - Not editable
+	rateLimitBps?: number; // The rate limit in bytes per second for message delivery - Editable
+	sampleFreq?: string; // The sampling frequency for metrics - Editable
+	maxWaiting?: number; // The maximum number of waiting messages - Editable
+	maxAckPending?: number; // The maximum number of unacknowledged messages - Editable
+	flowControl?: boolean; // Whether flow control is enabled - Editable
+	idleHeartbeat?: number; // The idle heartbeat interval - Editable
+	headersOnly?: boolean; // Whether to deliver only the headers of the message - Editable
+	maxBatch?: number; // The maximum batch size for message delivery - Editable
+	maxExpires?: number; // The maximum time duration (nanoseconds) a message can exist - Editable
+	maxBytes?: number; // The maximum size of a message in bytes - Editable
+	deliverSubject?: string; // The subject to deliver the messages to - Not editable
+	deliverGroup?: string; // The group to deliver the messages to - Not editable
+	inactiveThreshold?: number; // The threshold for marking a consumer as inactive - Editable
+	numReplicas: number; // The number of replicas for the consumer - Not editable
+	memStorage?: boolean; // Whether to use memory storage - Not editable
+}
+
+export enum DeliverPolicy {
+	DeliverAllPolicy = "all",
+	DeliverLastPolicy = "last",
+	DeliverNewPolicy = "new",
+	DeliverByStartSequencePolicy = "by_start_sequence",
+	DeliverByStartTimePolicy = "by_start_time",
+	DeliverLastPerSubjectPolicy = "last_per_subject"
+}
+
+export enum AckPolicy {
+	AckExplicitPolicy = "explicit",
+	AckAllPolicy = "all",
+	AckNonePolicy = "none"
+}
+
+export enum ReplayPolicy {
+	ReplayInstantPolicy = "instant",
+	ReplayOriginalPolicy = "original"
 }
 
 export interface SequenceInfo {
