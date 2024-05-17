41d252334230fe347794e8ffe894c12b2d067ff6
priolo <iorioivano@gmail.com>
2024-05-18 01:08:41 +0200

    [NUI-154] added pause feature to messages subscriptions

---
+++ frontend/src/icons/PauseIcon.tsx
@@ -0,0 +1,14 @@
+const PauseIcon = (props) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="14" height="14" viewBox="0 0 14 14"
+		stroke="currentColor"
+		fill="none"
+		{...props}
+	>
+		<path d="M9 3V11" strokeWidth="2"/>
+		<path d="M5 3V11" strokeWidth="2"/>
+	</svg>
+)
+
+export default PauseIcon
