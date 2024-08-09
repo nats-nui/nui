b66d5457f1c21a4014c8b067cf6636f7e6d22ada
pricelessrabbit <pricelessrabbit@gmail.com>
2024-08-09 18:41:31 +0200

    updated connection FE Form with additional fields needed

--- frontend/src/types/Connection.ts
+++ frontend/src/types/Connection.ts
@@ -1,48 +1,56 @@
 export interface Connection {
-	id?: string
-	name: string
-	hosts: string[]
-	subscriptions: Subscription[]
-	auth: Auth[]
-	status?: CNN_STATUS
+    id?: string
+    name: string
+    hosts: string[]
+    subscriptions: Subscription[]
+    auth: Auth[]
+    tlsAuth: TLSAuth
+    status?: CNN_STATUS
 }
 
 export interface Auth {
-	mode: AUTH_MODE
-	username?: string
-	password?: string
-	token?: string
-	jwt?: string
-	nKeySeed?: string
-	creds?: string
-	active?: boolean
+    mode: AUTH_MODE
+    username?: string
+    password?: string
+    token?: string
+    jwt?: string
+    nKeySeed?: string
+    creds?: string
+    active?: boolean
+}
+
+export interface TLSAuth {
+    enabled: boolean
+    certPath: string
+    keyPath: string
+    caPath: string
 }
 
 export interface Subscription {
-	subject: string
-	disabled?: boolean
-	favorite?: boolean
+    subject: string
+    disabled?: boolean
+    favorite?: boolean
 }
 
 export enum AUTH_MODE {
-	// use token field  
-	TOKEN = "auth_token",
-	// use username and password fields
-	USER_PASSWORD = "auth_user_password",
-	//use nkey public key and seed
-	NKEY = "auth_nkey",
-	// use jwt and nkey fields
-	JWT = "auth_jwt",
-	BEARER_JWT = "auth_jwt_bearer",
-	// use creds field (local path to a file)
-	CREDS_FILE = "auth_creds_file",
+    // use token field
+    TOKEN = "auth_token",
+    // use username and password fields
+    USER_PASSWORD = "auth_user_password",
+    //use nkey public key and seed
+    NKEY = "auth_nkey",
+    // use jwt and nkey fields
+    JWT = "auth_jwt",
+    BEARER_JWT = "auth_jwt_bearer",
+    // use creds field (local path to a file)
+    CREDS_FILE = "auth_creds_file",
 }
 
 
 export enum CNN_STATUS {
-	UNDEFINED = "undefined",
-	CONNECTED = "connected",
-	RECONNECTING = "reconnecting",
-	DISCONNECTED = "disconnected",
+    UNDEFINED = "undefined",
+    CONNECTED = "connected",
+    RECONNECTING = "reconnecting",
+    DISCONNECTED = "disconnected",
 }
 
