e5e5e779663fa2f6b526aba5e6b3447ed16e99a9
pricelessrabbit <pricelessrabbit@gmail.com>
2024-08-30 23:08:31 +0200

    added external flag and api and deliver fields to source stream dialog

--- frontend/src/components/stacks/streams/detail/cmp/EditSourceCmp.tsx
+++ frontend/src/components/stacks/streams/detail/cmp/EditSourceCmp.tsx
@@ -1,11 +1,12 @@
 import { Source } from "@/types/Stream"
-import { NumberInput, TextInput, Options } from "@priolo/jack"
-import { FunctionComponent } from "react"
+import {NumberInput, TextInput, Options, IconToggle, TooltipWrapCmp} from "@priolo/jack"
+import {FunctionComponent, useEffect, useState} from "react"
 
 
 
 interface Props {
 	source: Source
+	external?: boolean
 	readOnly?: boolean
 	allStream?: string[]
 	onChange: (source: Source) => void
@@ -20,25 +21,83 @@ const EditSourceCmp: FunctionComponent<Props> = ({
 
 	// HOOKS
 
+	//chose if render sources select dialog or external source dialog
+	const [isExternal, setIsExternal] = useState<boolean>(false)
+	useEffect(() => {
+		setIsExternal(source?.external != null)
+	}, []);
 	// HANDLER
 	const handleNameChange = (name: string) => onChange?.({ ...source, name })
 	const handleSequenceChange = (optStartSeq: number) => onChange?.({ ...source, optStartSeq })
 	//const handleStartTimeChange = (startTime: any) => onChange?.({ ...source, startTime: startTime })
 	const handleFilterSubjectChange = (filterSubject: string) => onChange?.({ ...source, filterSubject })
 
+	const handleIsExternalChange = (check: boolean) => {
+		source.external = null
+        if (check) {
+			source.external = {api: "", deliver: ""}
+		}
+		handleNameChange("")
+        setIsExternal(source?.external != null)
+	}
+
+    const handleExternalApiChange = (api: string) => onChange?.({ ...source, external: { ...source.external, api } })
+    const handleExternalDeliverChange = (deliver: string) => onChange?.({ ...source, external: { ...source.external, deliver } })
+
 	// RENDER
 	if (!source) return null
 
 	return <div className="jack-lyt-form var-dialog">
 		<div className="lyt-v">
-			<div className="jack-lbl-prop">NAME</div>
-			<Options<string> height={500}
-				value={source.name}
-				items={allStream}
-				readOnly={readOnly}
-				onSelect={handleNameChange}
-			/>
+			<div className="jack-cmp-h lbl-info-container">
+				<IconToggle
+					check={isExternal}
+					onChange={handleIsExternalChange}
+					readOnly={readOnly}
+				/>
+				<div className="jack-lbl-prop">EXTERNAL</div>
+				<TooltipWrapCmp className="lbl-info" children="?"
+								content="Source from external JS API (different account or domain)"
+				/>
+			</div>
 		</div>
+		<div className="lyt-v">
+			<div className="jack-lbl-prop">NAME</div>
+			{!isExternal
+				? <Options<string> height={500}
+								   value={source.name}
+								   items={allStream}
+								   readOnly={readOnly}
+								   onSelect={handleNameChange}
+				/>
+
+				: <TextInput
+					value={source.name}
+					onChange={handleNameChange}
+					readOnly={readOnly}
+				/>
+			}
+
+		</div>
+
+		{isExternal && <div className="lyt-v">
+			<div className="jack-lbl-prop">JS API</div>
+			<TextInput
+				value={source.external.api}
+				onChange={handleExternalApiChange}
+				readOnly={readOnly}
+			/>
+		</div>}
+
+		{isExternal && <div className="lyt-v">
+			<div className="jack-lbl-prop">DELIVER SUBJECT</div>
+			<TextInput
+				value={source.external.deliver}
+				onChange={handleExternalDeliverChange}
+				readOnly={readOnly}
+			/>
+		</div>}
+
 		<div className="lyt-v">
 			<div className="jack-lbl-prop">START SEQUENCE</div>
 			<NumberInput
@@ -47,8 +106,9 @@ const EditSourceCmp: FunctionComponent<Props> = ({
 				readOnly={readOnly}
 			/>
 		</div>
+
 		<div className="lyt-v">
-			<div className="jack-lbl-prop">FILTER SUBJECT</div>
+			<div className="jack-lbl-prop">FILTER SUBJECTS</div>
 			<TextInput
 				value={source.filterSubject}
 				onChange={handleFilterSubjectChange}
