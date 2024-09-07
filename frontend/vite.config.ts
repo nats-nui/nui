f6547cd13c17d630f077c322787f82cd3d82c14a
priolo <iorioivano@gmail.com>
2024-09-07 11:28:19 +0200

    updated jack library

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,7 +17,9 @@ export default defineConfig (( ) => {
         },
         resolve: {
             alias: {
-                '@': path.resolve(__dirname, './src')
+                '@': path.resolve(__dirname, './src'),
+                'react': path.resolve(__dirname, './node_modules/react'),
+                'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
             }
         }
     }
