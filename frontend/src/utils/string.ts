e9ea94700783eda0a586db921341c73614e6ff81
priolo <iorioivano@gmail.com>
2024-09-16 21:59:24 +0200

    Update comments in English

--- frontend/src/utils/string.ts
+++ frontend/src/utils/string.ts
@@ -1,33 +1,26 @@
 
 /**
- * Converte una "binary-string" in una "string"
+ * Converts a "binary-string" to a "string"
  */
 export function binaryStringToString(binaryString: string) {
-
-	// Primo passo: decodifica la stringa Base64 in una stringa binaria
 	//const binaryString = atob(str);
-
-	// Secondo passo: crea un Uint8Array dalla stringa binaria
-	const bytes = new Uint8Array(binaryString.length);
+	// create a Uint8Array from binary string
+	const bytes = new Uint8Array(binaryString.length)
 	for (let i = 0; i < binaryString.length; i++) {
-		bytes[i] = binaryString.charCodeAt(i);
+		bytes[i] = binaryString.charCodeAt(i)
 	}
-
-	// Terzo passo: decodifica l'array di byte UTF-8 in una stringa
-	return new TextDecoder().decode(bytes);
+	// decodes UTF-8 byte array into string
+	return new TextDecoder().decode(bytes)
 }
 
 /**
- * Converte una "string" in una "binary-string"
+ * Converts a "string" to a "binary-string"
  */
 export function stringToBinaryString(str: string): string {
-	// Primo passo: codifica la stringa in UTF-8
+	// encode the string in UTF-8
 	const utf8Bytes = new TextEncoder().encode(str);
-
-	// Secondo passo: converti l'array di byte in una stringa binaria
+	// convert byte array to binary string
 	const binaryString = String.fromCharCode(...utf8Bytes);
-
 	return binaryString
-	// // Terzo passo: codifica la stringa binaria in Base64
 	// return btoa(binaryString);
 }
