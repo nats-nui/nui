88b1d9bda4588548a2e29eeabca1544aa8f12c16
pricelessrabbit <pricelessrabbit@gmail.com>
2024-12-22 11:33:28 +0100

    updated entrypoint script to support explicit non-root user startup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,5 +9,12 @@ fi
 
 ARGS="--db-path=/db ${@}"
 
-# Switch to nui user and execute with all arguments
-exec su -c "/cmd/nui-web ${ARGS}" nui
+# Check if the current user UID is 1001 to support running as a different as non-root user
+# also the entrypoint
+if [ "$(id -u)" -eq 1001 ]; then
+  # Start the process directly
+  exec /cmd/nui-web ${ARGS}
+else
+  # Switch to nui user and execute with all arguments
+  exec su -c "/cmd/nui-web ${ARGS}" nui
+fi
