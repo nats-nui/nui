f10cdfa1aeff79dc7b6914685fad17096990216c
pricelessrabbit <pricelessrabbit@gmail.com>
2025-01-05 21:43:42 +0100

    added method to split context import paths

--- pkg/clicontext/clicontext.go
+++ pkg/clicontext/clicontext.go
@@ -51,6 +51,10 @@ type ImportedContextEntry struct {
 	Error           error                `json:"error"`
 }
 
+func SanitizePaths(paths string) []string {
+	return strings.Split(strings.ReplaceAll(paths, " ", ""), ",")
+}
+
 func NewImporter(l logging.Slogger) *CliImporter {
 	return &CliImporter{
 		l: l,
