315f9fb35ebb66947d2b763d251d6074a147d0c7
priolo <iorioivano@gmail.com>
2025-01-25 17:04:19 +0100

    chore: update @priolo/jack to version 0.2.6 and improve local storage handling in session management

--- frontend/src/utils/session/storage.ts
+++ frontend/src/utils/session/storage.ts
@@ -1,7 +1,7 @@
 import { Log } from "@/stores/log/utils"
 import { ViewState } from "@/stores/stacks/viewBase"
 import { Session } from "./types"
-import { DocState } from "@/stores/docs"
+import { DocState } from "@priolo/jack"
 
 
 
@@ -13,21 +13,25 @@ export function loadLocalStorage(): Session {
 	let drawerUuids: string[] = []
 	let menuUuids: string[] = []
 	try {
-		logs = JSON.parse(localStorage.getItem("logs"))
 		docsState = JSON.parse(localStorage.getItem("docs-state"))
 		allStates = JSON.parse(localStorage.getItem("cards-all"))
 		deckUuids = JSON.parse(localStorage.getItem("cards-deck-uuid"))
 		drawerUuids = JSON.parse(localStorage.getItem("cards-drawer-uuid"))
 		menuUuids = JSON.parse(localStorage.getItem("links-menu-uuid"))
+		logs = JSON.parse(localStorage.getItem("logs"))
 	} catch (e) { }
 	return { allStates, deckUuids, docsState, drawerUuids, menuUuids, logs }
 }
 
 export function saveLocalStorage(session: Session) {
-	localStorage.setItem("logs", JSON.stringify(session.logs))
 	localStorage.setItem("docs-state", JSON.stringify(session.docsState))
 	localStorage.setItem("cards-all", JSON.stringify(session.allStates))
 	localStorage.setItem("cards-deck-uuid", JSON.stringify(session.deckUuids))
 	localStorage.setItem("cards-drawer-uuid", JSON.stringify(session.drawerUuids))
 	localStorage.setItem("links-menu-uuid", JSON.stringify(session.menuUuids))
+	try {
+		localStorage.setItem("logs", JSON.stringify(session.logs))
+	} catch (e) {
+		console.error(e)
+	}
 }
