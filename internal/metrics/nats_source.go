efbd41b21aee32379210ff19779c2f2f5c640ebf
pricelessrabbit <pricelessrabbit@gmail.com>
2025-07-26 20:10:19 +0200

    added raw implementation and test of nats based source service

--- internal/metrics/nats_source.go
+++ internal/metrics/nats_source.go
@@ -4,21 +4,33 @@ import (
 	"context"
 	"encoding/json"
 	"github.com/nats-io/nats.go"
+	"github.com/nats-nui/nui/internal/connection"
 	"github.com/synadia-io/orbit.go/natssysclient"
 	"time"
 )
 
 type NatsSource struct {
-	conn *nats.Conn
-	sys  *natssysclient.System
+	conn     connection.Conn
+	natsConn *nats.Conn
+	sys      *natssysclient.System
 }
 
-func NewNatsSource() *NatsSource {
-	conn, _ := nats.Connect("nats://localhost:4222")
-	sys, _ := natssysclient.NewSysClient(conn)
+func NewNatsSource(conn connection.Conn) *NatsSource {
+	natsConn, _ := nats.Connect("nats://localhost:4222")
+	sys, _ := natssysclient.NewSysClient(natsConn)
 	return &NatsSource{
-		conn: conn,
-		sys:  sys,
+		conn:     conn,
+		natsConn: natsConn,
+		sys:      sys,
+	}
+}
+
+func NewWithNatsConnection(natsConn *nats.Conn) *NatsSource {
+	sys, _ := natssysclient.NewSysClient(natsConn)
+	return &NatsSource{
+		conn:     nil,
+		natsConn: natsConn,
+		sys:      sys,
 	}
 }
 
@@ -27,14 +39,14 @@ func (n NatsSource) FetchMetrics(ctx context.Context) (map[string]map[string]any
 	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
 	defer cancel()
 
-	varz, err := n.sys.Varz(ctx, "server_id", natssysclient.VarzEventOptions{})
+	varz, err := n.sys.Varz(ctx, n.natsConn.ConnectedServerId(), natssysclient.VarzEventOptions{})
 	if err != nil {
 		return metrics, err
 	}
 
 	metrics["varz"] = encodeToMap(varz.Varz)
 	options := natssysclient.ConnzEventOptions{ConnzOptions: natssysclient.ConnzOptions{Limit: 10000, Sort: natssysclient.ByLast}}
-	connz, err := n.sys.Connz(ctx, "server_id", options)
+	connz, err := n.sys.Connz(ctx, n.natsConn.ConnectedServerId(), options)
 	if err != nil {
 		return metrics, err
 	}
