02d6ae654f40af22e065ce96e7578e03730944bb
pricelessrabbit <pricelessrabbit@gmail.com>
2026-01-30 22:35:43 +0100

    refactored repo and add methods to get protos from specific paths

--- internal/protoschema/repo.go
+++ internal/protoschema/repo.go
@@ -3,5 +3,7 @@ package protoschema
 // ProtoRepo defines the interface for protobuf schema storage
 type ProtoRepo interface {
 	All() (map[string]*ProtoSchema, error)
+	AllInPath(path string) (map[string]*ProtoSchema, error)
 	GetById(id string) (*ProtoSchema, error)
+	GetByIdInPath(id string, path string) (*ProtoSchema, error)
 }
